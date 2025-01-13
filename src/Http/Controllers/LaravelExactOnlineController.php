2cfba6a0cd01e8b1fc74f3855fe1a7bdd335d1c7
Niels Rolleman <niels@fivefm.dev>
2025-01-13 14:37:21 +0100

    yep

--- src/Http/Controllers/LaravelExactOnlineController.php
+++ src/Http/Controllers/LaravelExactOnlineController.php
@@ -44,7 +44,7 @@ class LaravelExactOnlineController extends Controller
         Auth::loginUsingId(request()->get('user'));
 
         $config = LaravelExactOnline::loadConfig();
-
+        dd($config);
         $config->authorisationCode = request()->get('code');
         LaravelExactOnline::storeConfig($config);
 
