09f5a6ad790e47385e2295b10d997bb267f8f06a
Niels Rolleman <niels@fivefm.dev>
2025-01-14 11:16:51 +0100

    maybe fixed it

--- src/Http/Controllers/LaravelExactOnlineController.php
+++ src/Http/Controllers/LaravelExactOnlineController.php
@@ -17,43 +17,14 @@ class LaravelExactOnlineController extends Controller
     public function appAuthorize()
     {
         $connection = app()->make('Exact\Connection');
-        $userId = Auth::id();
-
-        if (!$userId) {
-            \Log::error('User not logged in');
-            abort(401, 'User not logged in');
-        }
-
-        // Generate and store the state
-        $state = encrypt(json_encode([
-            'user_id' => $userId,
-            'timestamp' => now()->timestamp,
-        ]));
-        session(['oauth_state' => $state]); // Store the state in the session for validation
-
-        \Log::info(json_encode([
-            'user_id' => $userId,
-            'timestamp' => now()->timestamp,
-        ]));
-        
-        // Append the state to the auth URL
-        $authUrl = $connection->getAuthUrl() . '&state=' . urlencode($state);
-
-        return ['url' => $authUrl];
+        return ['url' => $connection->getAuthUrl()];
     }
 
 
     public function appCallback()
     {
-        $state = request('state');
-
-        if (!$state || $state !== session('oauth_state')) {
-            \Log::error('Invalid state: ' . $state);
-            abort(401, 'User not logged in');
-        }
-
         // Decrypt the state to get the original data
-        $decodedState = json_decode(decrypt($state), true);
+        $decodedState = json_decode(decrypt(session('stoauth_stateate')), true);
 
         if (!$decodedState || !isset($decodedState['user_id'])) {
             \Log::error('Invalid state: ' . $state);
